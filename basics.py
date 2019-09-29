534040f2ae62913afb4f2719944d4c6e0249353b
mayankverma786 <mayankverma786@gmail.com>
2019-09-30 02:56:21 +0530

    Created a new file

---
+++ basics.py
@@ -0,0 +1,17 @@
+x = 4
+y = 5
+z = x+y
+print('Sum is ' + str(z))
+print("Hello Mayank")
+txt = "mayank"
+newTxt = " Hello World "
+#prints length of the string txt
+print("Length of the string is: " + str(len(txt)))
+#prints 2nd character of the string x = mayank
+print(txt[1])
+#
+print(txt[1:4])
+#Removes leading and trailing spaces
+print(newTxt.strip())
+#Convert the value of txt to upper case.
+print(txt.upper())
